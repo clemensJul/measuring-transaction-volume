76ab2652ffc6c769a2469804709a6b02a54c34b5
clemensblum <clemens.j.blum@gmail.com>
2026-02-04 14:11:45 +0100

    refactor: refactor of algorithm

--- src/processing/alg_cumulative_wealth_gain.py
+++ src/processing/alg_cumulative_wealth_gain.py
@@ -18,38 +18,30 @@ class CumulativeWealthGain:
         self.vertex_map[v2] = v - val
         return delta_gain
 
-    def update_map_only(self, v1, v2, val):
-        u = self.vertex_map.get(v1, 0)
-        v = self.vertex_map.get(v2, 0)
-        self.vertex_map[v1] = u + val
-        self.vertex_map[v2] = v - val
+    def rollback_txs(self, block):
+        for tx in block["transactions"]:
+            self.calc_gain(tx["to"], tx["from"], tx["usd_value"])
+        self.gain_total -= block["cached_gain"]
 
-    def rollback_map(self, txs):
-        for tx in txs:
-            self.update_map_only(tx["to"], tx["from"], tx["usd_value"])
 
-    def execute_txs(self, txs: list) -> int:
+    def execute_txs(self, block) -> int:
         block_gain = 0
-        for tx in txs:
+        for tx in block["transactions"]:
             g = self.calc_gain(tx["from"], tx["to"], tx["usd_value"])
             block_gain += g
-        return block_gain
+        self.previous_tx.append({
+            "timestamp": block["timestamp"],
+            "transactions": block["transactions"],
+            "cached_gain": block_gain,
+        })
+        self.gain_total += block_gain
 
     def run_on_block(self, block: dict) -> int:
         current_time = block["timestamp"]
         cutoff_time = current_time - self.two_delta
 
         while self.previous_tx and self.previous_tx[0]["timestamp"] < cutoff_time:
-            old_block_data = self.previous_tx.popleft()
-            self.gain_total -= old_block_data["cached_gain"]
-            self.rollback_map(old_block_data["transactions"])
+            self.rollback_txs(self.previous_tx.popleft())
+        self.execute_txs(block)
 
-        txs = block["transactions"]
-        current_block_gain = self.execute_txs(txs)
-        self.gain_total += current_block_gain
-        self.previous_tx.append({
-            "timestamp": current_time,
-            "transactions": txs,
-            "cached_gain": current_block_gain
-        })
         return self.gain_total
